2141292dbb0d149845bde7ef2e2f817075cfb9c6
Taylor Harris <thharris10@gmail.com>
2015-09-22 10:15:26 -0400

    upated models and views

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,15 @@
 class Restaurant < ActiveRecord::Base
   mount_uploader :image, ImageUploader
 
+  searchkick
+
   has_many :reviews
+
+  validates :name, :address, :phone, :website, :image, presence: true
+  validates :phone, format: { with: /\A\(\d{3}\) \d{3}-\d{4}\z/,
+    message: "must be in the format (123) 456-7890" }
+  validates :website, format: { with: /\Ahttps?:\/\/.*\z/,
+    message: "must start with http:// or https://" }
+  validates :address, format: { with: /\A\d+[^,]+,[^,]+, [A-Z]{2} \d{5}\z/,
+    message: "must be in the format 350 Fifth Avenue, New York, NY 10118" }
 end
