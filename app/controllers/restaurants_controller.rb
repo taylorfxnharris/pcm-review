7b48336583d9ef3fb039f1d7c4c18519d2c130ff
Taylor Harris <thharris10@gmail.com>
2015-09-22 09:55:42 -0400

    add fields to users

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,15 @@
 class RestaurantsController < ApplicationController
   before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:search, :index, :show]
+  before_action :check_user, except: [:search,  :index, :show]
+
+  def search
+    if params[:search].present?
+      @restaurants = Restaurant.search(params[:search])
+    else
+      @restaurants = Restaurant.all
+    end
+  end
 
   # GET /restaurants
   # GET /restaurants.json
@@ -10,13 +20,13 @@ class RestaurantsController < ApplicationController
   # GET /restaurants/1
   # GET /restaurants/1.json
   def show
-    @reviews = Review.where(restaurant_id: @restaurant.id).order("created at DESC")
-if @reviews.blank
-  @avg_rating = 0
-else
-    @avg_rating = @reviews.average(:rating).round(2)
+    @reviews = Review.where(restaurant_id: @restaurant.id).order("created_at DESC")
+    if @reviews.blank?
+      @avg_rating = 0
+    else
+      @avg_rating = @reviews.average(:rating).round(2)
+    end
   end
-end
 
   # GET /restaurants/new
   def new
@@ -71,6 +81,12 @@ end
     # Use callbacks to share common setup or constraints between actions.
     def set_restaurant
       @restaurant = Restaurant.find(params[:id])
+    end
+
+    def check_user
+      unless current_user.admin?
+        redirect_to root_url, alert: "Sorry, only admins can do that!"
+      end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
