97a9e9dad912607fb801c5f84b04aa265077b177
Taylor Harris <thharris10@gmail.com>
2015-09-23 07:45:33 -0400

    over and over3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,17 +6,5 @@ CarrierWave.configure do |config|
     aws_secret_access_key: ENV['aws_secret_access_key'],                         # required
     region:                'eu-west-2'
   }
-   # For testing, upload files to local `tmp` folder.
-  if Rails.env.test? || Rails.env.development?
-    config.storage = :file
-    config.enable_processing = false
-    config.root = "#{Rails.root}/tmp"
-  else
-    config.storage = :fog
-  end
-
-  # To let CarrierWave work on heroku
-  config.cache_dir = "#{Rails.root}/tmp/uploads"
-  config.fog_directory = ENV['taylorharrisproject2']
+  config.fog_directory = ENV['fog_directory']                          # required
 end
-
